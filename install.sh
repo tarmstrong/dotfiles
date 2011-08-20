72f423f63a02361cc9757360081186d7ecb265c9
Tavish Armstrong <tavisharmstrong@gmail.com>
2011-08-20 11:20:21 -0400

    Added .gitconfig, modified install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,39 @@ files='.vimrc .bashrc'
 
 for file in $files;
 do
-  echo "Linking $file to ~/$file"
-  ln -s $file ~/ 2>/dev/null
+  echo "Linking $PWD/$file to ~/$file"
+  ln -s $PWD/$file ~/ 2>/dev/null
   success=$?
-  [[ $success -eq 0 ]] || echo "Link already exists"
+  if [[ $success -eq 0 ]];
+  then
+    echo "Link made successfully."
+  else
+    echo "Shall I remove the existing $file?"
+    read remove
+    if [[ $remove == "Y" || $remove == "y" ]];
+    then
+      echo "removing the existing file."
+      rm ~/$file
+      echo "Linking again."
+      ln -s $PWD/$file ~/ 2>/dev/null
+      success=$?
+      if [[ ! $success -eq 0 ]];
+      then
+        echo "Link made successfully."
+      fi
+    fi
+  fi
   echo
 done
+
+echo "Making .vim swap directory."
+if [[ ! -d  ~/.vim/tmp/swap ]];
+then
+  mkdir -p ~/.vim/tmp/swap
+fi
+
+echo "Making .vim backup directory"
+if [[ ! -d  ~/.vim/tmp/backup ]];
+then
+  mkdir -p ~/.vim/tmp/backup
+fi
