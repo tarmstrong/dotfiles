c75602341231522514b967f7a7f959d7102349b1
Tavish Armstrong <tavish@evolvingweb.ca>
2011-09-21 12:41:21 -0400

    Added debugger.

--- install.sh
+++ install.sh
@@ -9,29 +9,17 @@ files='.vimrc .bashrc'
 for file in $files;
 do
   echo "Linking $PWD/$file to ~/$file"
-  ln -s $PWD/$file ~/ 2>/dev/null
-  success=$?
-  if [[ $success -eq 0 ]];
-  then
-    echo "Link made successfully."
-  else
-    echo "Shall I remove the existing $file?"
-    read remove
-    if [[ $remove == "Y" || $remove == "y" ]];
-    then
-      echo "removing the existing file."
-      rm ~/$file
-      echo "Linking again."
-      ln -s $PWD/$file ~/ 2>/dev/null
-      success=$?
-      if [[ ! $success -eq 0 ]];
-      then
-        echo "Link made successfully."
-      fi
-    fi
-  fi
+  rm ~/$file
+  ln -s $PWD/$file ~/
   echo
 done
+
+if [[ -d ~/.vim ]];
+then
+  rm -r ~/.vim
+fi
+
+ln -s $PWD/.vim ~/
 
 if [[ ! -d  ~/.vim/tmp/swap ]];
 then
